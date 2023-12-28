34f7c94c0afa0f47bac1bb884dabe4c1a20db16c
domlarocca <144866901+domlarocca@users.noreply.github.com>
2023-12-27 23:08:07 -0500

    Setup Fix 2
    Added SQL driver and dotenv
    Added .env
    Updated main to connect to database

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/KiranTheRam/CloudChat_Backend
 
 go 1.21.5
 
-require github.com/go-chi/chi/v5 v5.0.11 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.11 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,48 @@
-package CloudChat_Backend
+package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/go-chi/chi"
+	"os"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	
+	apiPort := os.Getenv("API_PORT")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	// Open a database connection
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUsername, dbPassword, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Attempt to ping the database to check the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome to the chat application!")
+		fmt.Fprint(w, "Backend Server Running...")
 	})
 
 	r.Get("/chat", func(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +51,6 @@ func main() {
 
 	// Add more routes as needed
 
-	http.ListenAndServe(":8080", r)
+	log.Printf("Server is running on :%s\n", apiPort)
+	http.ListenAndServe(":"+apiPort, r)
 }
